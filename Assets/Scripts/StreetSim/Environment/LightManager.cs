70c5790c5d56fa10f086490d3373c0ba580d5a48
Xi Liu <xi1331@126.com>
2023-02-01 22:10:37 -0500

    update

---
+++ Assets/Scripts/StreetSim/Environment/LightManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightManager : MonoBehaviour
+{
+    [SerializeField] private Light directionLight;
+    [SerializeField] private Light
+}
